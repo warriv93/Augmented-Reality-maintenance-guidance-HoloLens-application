9194faa2db2d2cf7100bab270b740c72dd61e0f9
Simon Gullstrand <warriv93@gmail.com>
2017-03-13 14:52:26 +0100

    Initial commit

---
+++ Assets/SimpleUI/Scripts/btnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnManager : MonoBehaviour {
+
+	public void testFirstBtn()
+    {
+        Debug.Log("Btn was pressed my man!");
+    }
+
+    public void ExitGameBtn()
+    {
+        Debug.Log("Exit btn was pressed!");
+        // kills application
+        Application.Quit();
+    }
+}
